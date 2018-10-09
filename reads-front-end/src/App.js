40248cf8ab964252cf84974afbbc6366f829ddab
Nicholas Lee <nicholaslee@Nicholass-MacBook-Air-2.local>
2018-10-09 13:51:54 -0600

    created basic app to start project with

---
+++ reads-front-end/src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+
+
+class App extends Component {
+  constructor(props)  {
+    super(props)
+    this.state = {
+      //this refers to the object its self
+    }
+  }
+  
+  
+
+  
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+}
+
+
+export default App;
